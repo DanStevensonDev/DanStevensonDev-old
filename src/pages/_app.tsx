39fc4a84d570169ed111626c7f84bcfb87151cf2
DanStevensonDev <danstevensonco@gmail.com>
2023-08-02 14:41:27 +0100

    add Google Fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
+import { Sen } from 'next/font/google'
+ 
+const font = Sen({ weight: '400', subsets: []})
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (<main className={font.className}>
+      <Component {...pageProps} />
+    </main>)
 };
 
 export default api.withTRPC(MyApp);
